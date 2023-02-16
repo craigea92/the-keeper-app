2bb7f3cd0820b99e9ad11285d6dfd3ea5f51884a
Craig Eaton <craigea92@gmail.com>
2023-02-16 09:53:51 +0000

    Added Header, Footer and Note components into the App file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <h1>Hello World!</h1>,
+  <App />,
   document.getElementById("root")
 );
