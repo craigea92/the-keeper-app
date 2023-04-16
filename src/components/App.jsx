e57e623b3bf6c1588bca8c80b644da9642d63b86
Craig Eaton <craigea92@gmail.com>
2023-04-16 10:43:27 +0100

    Added an id for each note using index

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -25,9 +25,11 @@ function App() {
     <div>
       <Header />
       <CreateArea onAdd={addNote} />
-      {notes.map((noteItem) => {
+      {notes.map((noteItem, index) => {
         return (
           <Note
+            key={index}
+            id={index}
             title={noteItem.title}
             content={noteItem.content}
             onDelete={deleteNote}
