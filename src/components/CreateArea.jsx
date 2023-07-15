7bf1020fcbc242d318395b2e43d28dfe9fcd8556
Craig Eaton <craigea92@gmail.com>
2023-07-15 19:20:12 +0100

    Added comments to CreateArea.jsx

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
 
 function CreateArea(props) {
+  // Initializing state variable 'note' and its setter function 'setNote' using the 'useState' hook
   const [note, setNote] = useState({
     title: "",
     content: "",
   });
 
+  // Event handler function to handle changes in the input fields
   function handleChange(event) {
+    // Extracting the 'name' and 'value' properties from the target element of the event
     const { name, value } = event.target;
+
+    // Updating 'note' object by creating a new object that includes the previous note properties and the updated property
     setNote((prevNote) => {
       return {
         ...prevNote,
@@ -16,15 +21,22 @@ function CreateArea(props) {
     });
   }
 
+  // Event handler function to handle form submission
   function submitNote(event) {
+    // Calling the 'onAdd' function passed as a prop and passing the 'note' object
     props.onAdd(note);
+
+    // Resetting the 'note' object to its initial state
     setNote({
       title: "",
       content: "",
     });
+
+    // Preventing the default form submission behavior
     event.preventDefault();
   }
 
+  // Render function of the CreateArea component
   return (
     <div>
       <form>
