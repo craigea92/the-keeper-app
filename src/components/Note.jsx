17c297991288ae0b6f9bede4a7470d18ee29aa77
Craig Eaton <craigea92@gmail.com>
2023-07-16 09:56:12 +0100

    Added comments to Note.jsx

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 function Note(props) {
+  // Event handler function to handle the delete button click
   function handleClick() {
+    // Calling the 'onDelete' function passed as a prop and passing the 'id' prop of the Note component
     props.onDelete(props.id);
   }
 
+  // Render function of the Note component
   return (
     <div className="note">
       <h1>{props.title}</h1>
